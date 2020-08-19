f985bd3216909ef9724757c60d19a0284a61c3ee
wagowsky <advakond@gmail.com>
2020-08-19 09:53:22 +0200

    created adduser pages

--- src/components/layout/Members2.js
+++ src/components/layout/Members2.js
@@ -4,47 +4,45 @@ import skater from '../../assets/skater.png';
 import { Link } from 'react-router-dom';
 import AddUser from './AddUser';
 
-
 const Members2 = () => {
   return (
     <div className=' border'>
-    <div className='header '>
-      <h1 className=' text details'>WG name</h1>
-      <div className='underline'></div>
-    </div>
-
-    <div className='content '>
-      <div className='user-gallery'>
-        <Link to='userdetails'>
-          <div className='user'>
-            <img src={skater} className='avatar' alt='' />
-            <p className=' text details'>Balazs</p>
-            <p className=' text details'>xp 375</p>
-          </div>
-        </Link>
-        <div className='user'>
-          <AddUser />
-          <p className=' text details'>Balazs</p>
-          <p className=' text details'>xp 375</p>
-        </div>
-       
+      <div className='header '>
+        <h1 className=' text details'>WG name</h1>
+        <div className='underline'></div>
       </div>
 
-      <Link to='shop'>
-        <button className='input input-green'>
-          <p className=' text '>shop</p>
-        </button>
-      </Link>
-      <button className='input input-green'>
-        <p className=' text '>enter arena</p>
-      </button>
-    </div>
+      <div className='content '>
+        <div className='user-gallery'>
+          <Link to='userdetails'>
+            <div className='user'>
+              <img src={skater} className='avatar' alt='' />
+              <p className=' text details'>Balazs</p>
+              <p className=' text details'>xp 375</p>
+            </div>
+          </Link>
+          <Link to='registeruser'>
+            <div className='user'>
+              <AddUser />
+            </div>
+          </Link>
+        </div>
 
-    <div className='footer'>
-      <Speaker />
+        <Link to='shop'>
+          <button className='input input-green'>
+            <p className=' text '>shop</p>
+          </button>
+        </Link>
+        <button className='input input-green'>
+          <p className=' text '>enter arena</p>
+        </button>
+      </div>
+
+      <div className='footer'>
+        <Speaker />
+      </div>
     </div>
-  </div>
   );
-}
+};
 
 export default Members2;
